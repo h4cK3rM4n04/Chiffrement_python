7170e4415f6495f801a0fbbf6e432f753036cc1e
h4cK3rM4n04 <eliemanoarm@gmail.com>
2023-10-21 10:27:39 +0200

    Chiffrement modifié

--- atbash.py
+++ atbash.py
@@ -4,7 +4,7 @@ import string as s
 x = s.ascii_letters+' '+s.punctuation
 y = x[::-1]
 
-word = ']4|W3/U4=04'
+word = '+<-/`G;.`,{<G`.-qG]4|W3/U4=04'
 transformation = str.maketrans(x, y)
 result = str.translate(word, transformation)
 
